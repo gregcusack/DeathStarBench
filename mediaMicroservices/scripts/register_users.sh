311d46a8b647578db13113ca8e66718c65b49aeb
gregcusack <gcusack@netflix.com>
2020-08-28 16:23:09 -0600

    make dsb more configurable and easier to run

--- mediaMicroservices/scripts/register_users.sh
+++ mediaMicroservices/scripts/register_users.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+ipaddr=$1
+
 for i in {1..1000}; do
   curl -d "first_name=first_name_"$i"&last_name=last_name_"$i"&username=username_"$i"&password=password_"$i \
-      http://10.68.24.13:8080/wrk2-api/user/register
+      http://$ipaddr:8080/wrk2-api/user/register
 done
